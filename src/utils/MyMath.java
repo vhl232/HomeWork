9369245cb91430000975ef87e7e02229d26f82b6
vhl232 <vhl232@gmail.com>
2017-07-04 21:09:36 +0300

    first method for testing ready

---
+++ src/utils/MyMath.java
@@ -0,0 +1,10 @@
+package utils;
+
+/**
+ * Created by spasynkov on 04.07.17.
+ */
+public class MyMath {
+    public static int aPlusB(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/utils/Testing.java
@@ -0,0 +1,31 @@
+package utils;
+
+/**
+ * Created by vladimir on 04.07.17.
+ */
+public class Testing {
+    public static void main(String[] args) {
+        test1();
+        test2();
+        test3();
+    }
+
+    private static void test1() {
+        System.out.print("Test 1: ");
+        int result = MyMath.aPlusB(2, 3);
+
+
+        System.out.println("ok");
+
+        String textOshibki = "";
+        System.out.println("Error: " + textOshibki);
+    }
+
+    private static void test2() {
+
+    }
+
+    private static void test3() {
+
+    }
+}
